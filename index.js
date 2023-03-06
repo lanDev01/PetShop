547997dae54e9bc640a30e069f5a302c55882543
lanDev01 <alnjr0001@outlook.com>
2023-03-06 16:57:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function pesquisacep(cep) {
+    let cepfinal = cep
+
+    if (cepfinal != "") {
+        let script = document.createElement('script')
+        script.src = 'https://viacep.com.br/ws/' + cepfinal + '/json/?callback=callback_name'
+        document.body.appendChild(script)
+    }
+    else {
+        limparcampos()
+    }
+}
+
+function callback_name(objetocep) {
+    if (!("erro" in objetocep)) {
+        document.getElementById('rua').value = (objetocep.logradouro)
+        document.getElementById('cidade').value = (objetocep.localidade)
+        document.getElementById('estado').value = (objetocep.uf)
+    }
+}
+
+function limparcampos() {
+    document.getElementById('rua').value = ""
+    document.getElementById('cidade').value = ""
+    document.getElementById('estado').value = ""
+}
